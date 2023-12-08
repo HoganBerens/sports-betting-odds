f9faa2001baa68c7c33ee35a79802f2bc25687c2
HoganBerens <berenshogan@gmail.com>
2023-12-08 12:02:55 -0600

    add dummy cron-job

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,18 @@ import axios from "axios";
 const Dashboard = () => {
   const [todaysOdds, setTodaysOdds] = useState([]);
 
+  function getOdds() {
+    axios
+      .get("https://nhl-score-api.herokuapp.com/api/scores?startDate=2023-12-07&endDate=2023-12-07")
+      .then((response) => {
+        console.log(response.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getOdds();
   return <div>Dashboads</div>;
 };
 
--- utils/node-cron.js
+++ utils/node-cron.js
@@ -19,7 +19,35 @@ function cronJob() {
         )
         .then((response) => {
           controller.create(response.data);
-          console.log(response.data, "done");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    {
+      scheduled: true,
+      timezone: "America/Chicago",
+    }
+  );
+}
+
+function newCronJob() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  let start = new Date();
+  let end = new Date();
+  start.setHours(start.getHours() + 8);
+  end.setHours(end.getHours() + 25);
+  cron.schedule(
+    "* 30 12 * * *",
+    () => {
+      axios
+        .get(
+          `https://api.the-odds-api.com/v4/sports/icehockey_nhl/odds/?apiKey=${API_KEY}&regions=us&markets=h2h,spreads&oddsFormat=american&bookmakers=draftkings&commenceTimeFrom=${start.toISOString().split(".")[0] + "Z"}&commenceTimeTo=${
+            end.toISOString().split(".")[0] + "Z"
+          }`
+        )
+        .then((response) => {
+          controller.create(response.data);
         })
         .catch((error) => {
           console.log(error);
@@ -34,4 +62,5 @@ function cronJob() {
 
 module.exports = {
   cronJob,
+  newCronJob,
 };
